e29ad35f8d844a948760e4848deeadc73f070ff8
Margarida Silva <margaridaocs@ua.pt>
2019-05-15 13:53:32 +0100

    Capture script displays end time

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 if [ $# -eq 0 ]; then
 	echo Usage:   ./run.sh name
-	echo          ./run.sh name index [when starting count from an offset]
 	echo Example: ./run.sh browsing
-	echo "$HOSTNAME"_browsing_0.pcap
+	echo Will create file "$HOSTNAME"_browsing_1557922782351.pcap
 	exit
 fi
 if [ $# -eq 2 ]; then
-	cnt=$2
+	duration=$2
 else 
-	cnt=0
+	duration=600
 fi
 
-while [ true ]; do 
-	tshark -w "$HOSTNAME"_$1_$cnt.pcap -a duration:600
-	cnt=$(($cnt+1))
+while [ true ]; do
+	tstamp=$(($(date +%s%N)/1000000))
+	echo Writing to file "$HOSTNAME"_$1_$tstamp.pcap
+	echo Capture started at $(date +"%T"), ends at $(date -d '($duration) seconds' +"%T")
+	tshark -w "$HOSTNAME"_$1_$tstamp.pcap -a duration:$duration
+	echo
 done
