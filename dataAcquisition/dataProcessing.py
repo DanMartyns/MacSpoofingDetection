b641f226fed75dfb08f097871b9c59e0b36e8406
Daniel Martins <danielmcoelho@ua.pt>
2019-06-17 09:59:01 +0100

    dataProcessing updated

--- dataAcquisition/dataProcessing.py
+++ dataAcquisition/dataProcessing.py
@@ -20,18 +20,20 @@ observation = None
 result = np.zeros(22)
 
 def observation_analyse(ob_window, file_obj):
+    info = []
+    tmpS = 0
+    tmpD = 0
+    print(ob_window)
+    
     print("\nMedia para cada métrica :")
     print([float(x) for x in np.mean(ob_window,axis=0)])
     result[0:8] = [float(x) for x in np.mean(ob_window,axis=0)]
     print("\nDesvio padrão para cada métrica :")    
     print([float(x) for x in np.std(ob_window, axis=0)])
     result[8:16] = [float(x) for x in np.std(ob_window, axis=0)]
-    info = []
-    tmpS = 0
-    tmpD = 0
-    # TODO: revise what's going on here
+
     for x in ob_window[:,0] :
-        #print(x)
+        print(x)
         x = int(x)
         if x==0 and tmpD > 0 :
             tmpS += 1
@@ -43,46 +45,49 @@ def observation_analyse(ob_window, file_obj):
             tmpD += 1
             info.append([0,tmpS])
             tmpS = 0            
-        elif x != 0 :
-            tmpD += 1     
+        elif x != 0 :         
+            tmpD += 1
+    
+    #In case of only data or only silences
+    info.append([1,tmpD])
+    info.append([0,tmpS])     
 
-    if info != []:
-        tmp_data = [x[1] for x in info if x[0] == 1 ]
-        tmp_silence = [x[1] for x in info if x[0] == 0 ]
-        avg_data = 0
-        var_data = 0
-        avg_silence = 0
-        var_silence = 0
-        if len(tmp_data) > 0:
-            avg_data = statistics.mean(tmp_data)
-            if len(tmp_data) > 1:
-                var_data = statistics.variance([x[1] for x in info if x[0] == 1 ])
-        if len(tmp_silence) > 0:
-            avg_silence = statistics.mean(tmp_silence)
-            if len(tmp_silence) > 1:
-                var_silence = statistics.variance([x[1] for x in info if x[0] == 0 ])
+    print(info)    
+    # Regardless of the time of a sampling window, count how many data jails there are. This gives us the time of data and the time of silence
+    tmp_data = [x[1] for x in info if x[0] == 1 ]
+    tmp_silence = [x[1] for x in info if x[0] == 0 ]
+    
+    avg_data = 0
+    var_data = 0
+    avg_silence = 0
+    var_silence = 0
+    if len(tmp_data) > 0:
+        avg_data = statistics.mean(tmp_data)
+        if len(tmp_data) > 1:
+            var_data = statistics.variance([x[1] for x in info if x[0] == 1 ])
+    if len(tmp_silence) > 0:
+        avg_silence = statistics.mean(tmp_silence)
+        if len(tmp_silence) > 1:
+            var_silence = statistics.variance([x[1] for x in info if x[0] == 0 ])
 
-        #print("\n( [ Time type , How many consecutive ] ) \n Time Type : 0 - Silence time || 1 - Data time")
-        #print(info)
-        print("\nNumero de dados -> %d" %sum(x[0]==1 for x in info))
-        result[16] = sum(x[0]==1 for x in info)
-        print("\nNumero de silencios -> %d"%sum(x[0]==0 for x in info))
-        result[17] = sum(x[0]==0 for x in info)
-        print("\nTempo de dados medio : %.2f"%avg_data)
-        result[18] = avg_data
-        print("\nTempo de silêncio medio : %.2f"%avg_silence)
-        result[19] = avg_silence
-        print("\nVariância do tempo de dados : %.2f"%var_data)
-        result[20] = var_data
-        print("\nVariância do tempo de silêncios : %.2f"%var_silence)
-        result[21] = var_silence
-        print("\nMatriz de Saída ")
-        print(result)
-        for r in result:
-            file_obj.write(str(r) + ' ')
-        file_obj.write("\n")
-    else :
-        print("\nThe file was little information")
+    #print("\n( [ Time type , How many consecutive ] ) \n Time Type : 0 - Silence time || 1 - Data time")
+    print("\nNumero de dados -> %d" %sum(x[0]==1 for x in info  if x[1] != 0))
+    result[16] = sum(x[0]==1 for x in info  if x[1] != 0)
+    print("\nNumero de silencios -> %d"%sum(x[0]==0 for x in [x for x in info  if x[1] != 0]))
+    result[17] = sum(x[0]==0 for x in [x for x in info  if x[1] != 0] )
+    print("\nTempo de dados medio : %.2f"%avg_data)
+    result[18] = avg_data
+    print("\nTempo de silêncio medio : %.2f"%avg_silence)
+    result[19] = avg_silence
+    print("\nVariância do tempo de dados : %.2f"%var_data)
+    result[20] = var_data
+    print("\nVariância do tempo de silêncios : %.2f"%var_silence)
+    result[21] = var_silence
+    print("\nMatriz de Saída ")
+    print(result)
+    for r in result:
+        file_obj.write(str(r) + ' ')
+    file_obj.write("\n")
 
 def readFile(filetoread):
     global observation
